17d20ab27ce25161862b43a3def5f9144364f2fa
daffa2555 <daffaanan11@gmail.com>
2026-07-26 05:09:29 +0800

    fix: extract string value inside quotes for output display

--- vm/wasm_main.c
+++ vm/wasm_main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <emscripten/emscripten.h>
 #include "vx.h"
 
@@ -6,5 +7,18 @@ EMSCRIPTEN_KEEPALIVE
 void run_from_js(const char* code) {
     if (code == NULL) return;
 
-    printf("Code Vidyax Run: %s\n", code);
+ 
+    const char *first_quote = strchr(code, '"');
+    if (first_quote != NULL) {
+        const char *second_quote = strchr(first_quote + 1, '"');
+        if (second_quote != NULL) {
+
+            size_t len = second_quote - (first_quote + 1);
+            printf("%.*s\n", (int)len, first_quote + 1);
+            return;
+        }
+    }
+
+    
+    printf("%s\n", code);
 }
