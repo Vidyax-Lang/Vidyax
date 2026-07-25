04405f34529544fbad468f25e9b1625a9a031592
daffa2555 <daffaanan11@gmail.com>
2026-07-26 05:04:49 +0800

    anjaiiii bahasa c mantapppp lah pen banting laptop rasanya

--- vm/wasm_main.c
+++ vm/wasm_main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <emscripten/emscripten.h>
+#include "vx.h"
 
 EMSCRIPTEN_KEEPALIVE
 void run_from_js(const char* code) {
+    if (code == NULL) return;
+
     printf("Code Vidyax Run: %s\n", code);
 }
